b477949637678412820cab825a248a16a7829700
james-monahan <monahan.jpm.fr@gmail.com>
2022-05-03 09:15:41 +0200

    basic queries

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const users = require('./data').users;
+const me = users[0];
+const typeDefs = gql`
+    type Query {
+        me: User
+        users: [User]
+        user(id: Int!): User
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        }
+    `;
+
+const resolvers = {
+    Query: {
+        users: () => users,
+        //user: (parent, args, context, info)
+        user: (parent, { id }) => {
+            const user = users.filter(user => user.id === id);
+            return user[0];
+        },
+        me: () => me
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
-const { ApolloServer } = require('apollo-server-express');
 
-const typeDefs = null;
-const resolvers = null;
-
-const server = new ApolloServer ({
-    typeDefs,
-    resolvers
+server.start().then(res => {
+    server.applyMiddleware({ app });
+    app.listen({ port: 3000 }, () =>
+        console.log("nice")
+    )
 })
-server.applyMiddleware({ app });
-
-
-app.listen(3000, () => console.info('Apollo GraphQL server is running on port 3000'));
-
---
+++ data.js
@@ -0,0 +1,14 @@
+let users = [{
+    id: 1,
+    name: 'James'
+}, {
+    id: 2,
+    name: 'Jim'
+}, {
+    id: 3,
+    name: 'Jamie'
+}];
+
+module.exports = {
+    users
+};
