e34f43ca4a11297efc8f0e0e49544c0ee2e8a1ca
james-monahan <monahan.jpm.fr@gmail.com>
2022-05-06 10:20:54 +0200

    relationships

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
+//section 3 challenge
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const users = require('./data').users;
+const cars = require('./data').cars;
 const me = users[0];
 const typeDefs = gql`
     type Query {
         me: User
         users: [User]
         user(id: Int!): User
+        cars: [Car]
+        car(id: Int!): Car
     }
 
     type User {
         id: ID!
         name: String!
         }
+
+    type Car {
+        id: ID!
+        make: String!
+        model: String!
+        color: String
+        owner: User!
+        }
     `;
 
 const resolvers = {
     Query: {
         users: () => users,
-        //user: (parent, args, context, info)
         user: (parent, { id }) => {
             const user = users.filter(user => user.id === id);
             return user[0];
         },
-        me: () => me
+        me: () => me,
+        cars: () => cars,
+        car: (parent, { id }) => {
+            const user = cars.filter(car => car.id === id);
+            return cars[0];
+        }
+    },
+    Car: {
+        owner: parent => users[parent.ownedBy - 1]
     }
 };
 
--- data.js
+++ data.js
@@ -9,6 +9,27 @@ let users = [{
     name: 'Jamie'
 }];
 
+let cars = [{
+    id: 1,
+    make: 'Tesla',
+    model: 'Model S',
+    color: 'black',
+    ownedBy: 1
+}, {
+    id: 2,
+    make: 'Faraday',
+    model: 'FF 91',
+    color: 'red',
+    ownedBy: 2
+}, {
+    id: 3,
+    make: 'Lucid',
+    model: 'Air',
+    color: 'apple',
+    ownedBy: 3
+}];
+
 module.exports = {
-    users
+    users,
+    cars
 };
